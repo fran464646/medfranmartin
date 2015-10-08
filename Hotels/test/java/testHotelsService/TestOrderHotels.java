129aab516b7075f7973e8a28a4d76ec3c68f91ac
martin.quinteiro <martin.quinteiro@778da45d-28bb-4820-94bf-61f22ca371e3>
2015-10-08 19:53:31 +0000

    Implementado parte de los test de Ordenación de los hoteles

---
+++ Hotels/test/java/testHotelsService/TestOrderHotels.java
@@ -0,0 +1,41 @@
+package testHotelsService;
+
+import static org.junit.Assert.*;
+import hotel.Hotel;
+import hotel.HotelDAO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class TestOrderHotels {
+
+	HotelDAO hoteldao = new HotelDAO();
+	ArrayList<String> keys = new ArrayList<String>();
+	ArrayList<String> values = new ArrayList<String>();
+	List<Hotel> hoteles_BD = new ArrayList<Hotel>();
+	
+	@Test
+	public void testOrderHotelsByName() {
+		int i = 0;
+		int tamano = hoteles_BD.size();
+		if (tamano>1){
+			for (int j = 1;j<tamano;j++){
+				assertTrue(hoteles_BD.get(i).getNombre().compareToIgnoreCase(hoteles_BD.get(j).getNombre())<=0);
+			}
+		} else assertTrue(true);
+	}
+	
+	@Test
+	public void testOrderHotelsByCategory() {
+		int i = 0;
+		int tamano = hoteles_BD.size();
+		if (tamano>1){
+			for (int j = 1;j<tamano;j++){
+				assertTrue(Integer.compare(hoteles_BD.get(i).getCategoria(),hoteles_BD.get(j).getCategoria())<=0);
+			}
+		} else assertTrue(true);
+	}
+
+}
