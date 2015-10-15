955639a43f7e751f8a5236441aefefdc72fe3701
martin.quinteiro <martin.quinteiro@778da45d-28bb-4820-94bf-61f22ca371e3>
2015-10-15 12:35:43 +0000

    modificado test

---
+++ Hotels/test/java/testHotelsService/TestCheckAvailabilityHotel.java
@@ -0,0 +1,26 @@
+package testHotelsService;
+
+import static org.junit.Assert.assertTrue;
+import hotel.HotelDAO;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import tipoHabitacion.TipoHabitacion;
+
+public class TestCheckAvailabilityHotel {
+	List<TipoHabitacion> tipos = new ArrayList<TipoHabitacion>();
+	HotelDAO hoteldao;
+	@Test
+	public void testCheckAvailability() throws ParseException {
+		tipos = hoteldao.getHotelRooms("1", "14/10/2015", "15/10/2015");
+		assertTrue(Long.compare(tipos.get(0).getId(),1)==0);
+		assertTrue(Long.compare(tipos.get(1).getId(),2)==0);
+
+	}
+	
+	
+}
