6cf701ecbdf457167cd8f24f8cedbeddf4b64bae
martin.quinteiro <martin.quinteiro@778da45d-28bb-4820-94bf-61f22ca371e3>
2015-10-16 07:55:30 +0000

    Corrección de errores en testorderhotels y testviewhotel y añadido el test checkViewServicesRoom

--- trunk/Hotels/test/java/testHotelsService/TestViewHotel.java
+++ trunk/Hotels/test/java/testHotelsService/TestViewHotel.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 import org.junit.Test;
 
+import servicio.Servicio;
 import tipoHabitacion.TipoHabitacion;
 
 public class TestViewHotel {
@@ -20,18 +21,9 @@ public class TestViewHotel {
 		tipos=hoteldao.getHotelRooms("1", "14/10/2015", "15/10/2015");
 		Iterator itr = tipos.iterator();
        Object[] obj = (Object[]) itr.next();
-       //now you have one array of Object for each row
        assertTrue(Long.compare(Long.valueOf(String.valueOf(obj[0])),1)==0);
        obj = (Object[]) itr.next();
        assertTrue(Long.compare(Long.valueOf(String.valueOf(obj[0])),2)==0);
 	}
-	
-	@Test
-	public void testCheckViewHabitacion() {
-		TipoHabitacion result;
-		result = hoteldao.getHabitacion("1");
-		assertTrue(Long.compare(result.getId(),1l)==0);
-	}
-	
 	
 }
