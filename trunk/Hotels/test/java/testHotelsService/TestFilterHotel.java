940404ede070077050f6f5678ccd712313d583c4
martin.quinteiro <martin.quinteiro@778da45d-28bb-4820-94bf-61f22ca371e3>
2015-10-16 09:53:14 +0000

    añadidos testCheckFilterHotelsByNumberOfAvailableRooms e testCheckFilterHotelsByServices

--- trunk/Hotels/test/java/testHotelsService/TestFilterHotel.java
+++ trunk/Hotels/test/java/testHotelsService/TestFilterHotel.java
@@ -15,7 +15,7 @@ public class TestFilterHotel {
 	HotelDAO hoteldao = new HotelDAO();
 
 	@Test
-	public void testCheckFilterHotelByPrice() {
+	public void testCheckFilterHotelsByPrice() {
 		ArrayList<String> keys = new ArrayList<String>();
 		ArrayList<String> values = new ArrayList<String>();
 		keys.add("preciominimo");
@@ -24,7 +24,35 @@ public class TestFilterHotel {
 		values.add("120");
 		List<Hotel> hoteles = new ArrayList<Hotel>();
 		hoteles = hoteldao.getHotels(keys,values);
-	       assertTrue(Long.compare(hoteles.get(0).getId(),3l)==0);
-	       assertTrue(Long.compare(hoteles.get(1).getId(),7l)==0);
+	    assertTrue(Long.compare(hoteles.get(0).getId(),3l)==0);
+	    assertTrue(Long.compare(hoteles.get(1).getId(),7l)==0);
 	}
+	
+	@Test
+	public void testCheckFilterHotelsByServices() {
+		ArrayList<String> keys = new ArrayList<String>();
+		ArrayList<String> values = new ArrayList<String>();
+		keys.add("servicio");
+		values.add("comida");
+		List<Hotel> hoteles = new ArrayList<Hotel>();
+		hoteles = hoteldao.getHotels(keys,values);
+	    assertTrue(Long.compare(hoteles.get(0).getId(),1l)==0);
+	}
+	
+	@Test
+	public void testCheckFilterHotelsByNumberOfAvailableRooms() {
+		ArrayList<String> keys = new ArrayList<String>();
+		ArrayList<String> values = new ArrayList<String>();
+		keys.add("numeroHabitaciones");
+		values.add("3");
+		List<Hotel> hoteles = new ArrayList<Hotel>();
+		hoteles = hoteldao.getHotels(keys,values);
+	    assertTrue(Long.compare(hoteles.get(0).getId(),3l)==0);
+	    assertTrue(Long.compare(hoteles.get(1).getId(),4l)==0);
+	    assertTrue(Long.compare(hoteles.get(2).getId(),5l)==0);
+	    assertTrue(Long.compare(hoteles.get(3).getId(),6l)==0);
+	    assertTrue(Long.compare(hoteles.get(4).getId(),7l)==0);
+	    assertTrue(Long.compare(hoteles.get(5).getId(),8l)==0);
+	}
+	
 }
