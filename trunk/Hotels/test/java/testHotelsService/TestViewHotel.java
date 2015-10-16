aafe5338dc8abba0c309f831d02ead5ad48f4df6
martin.quinteiro <martin.quinteiro@778da45d-28bb-4820-94bf-61f22ca371e3>
2015-10-16 08:35:19 +0000

    añadido testCheckServicesHotel

--- trunk/Hotels/test/java/testHotelsService/TestViewHotel.java
+++ trunk/Hotels/test/java/testHotelsService/TestViewHotel.java
@@ -10,13 +10,14 @@ import java.util.List;
 
 import org.junit.Test;
 
+import regimen.Regimen;
 import servicio.Servicio;
 import tipoHabitacion.TipoHabitacion;
 
 public class TestViewHotel {
 	HotelDAO hoteldao = new HotelDAO();
 	@Test
-	public void testCheckAvailability() throws ParseException {
+	public void testCheckAvailabilityHotel() throws ParseException {
 		List<TipoHabitacion> tipos = new ArrayList<TipoHabitacion>();
 		tipos=hoteldao.getHotelRooms("1", "14/10/2015", "15/10/2015");
 		Iterator itr = tipos.iterator();
@@ -24,6 +25,14 @@ public class TestViewHotel {
        assertTrue(Long.compare(Long.valueOf(String.valueOf(obj[0])),1)==0);
        obj = (Object[]) itr.next();
        assertTrue(Long.compare(Long.valueOf(String.valueOf(obj[0])),2)==0);
+	}
+	
+	@Test
+	public void testCheckServicesHotel() {
+		List<Servicio> servicios = new ArrayList<Servicio>();
+		servicios = hoteldao.getServiciosHotel("1");
+	       assertTrue(Long.compare(servicios.get(0).getId(),1l)==0);
+		}
 	}
 	
 }
